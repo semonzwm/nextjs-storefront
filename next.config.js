892c2835d351a256b18fcc411b31a28c25fd6566
semonzwm <toolsapply@hotmail.com>
2026-06-09 08:22:24 +0800

    fix: add standalone output mode

--- next.config.js
+++ next.config.js
@@ -2,18 +2,16 @@ const checkEnvVariables = require("./check-env-variables")
 
 checkEnvVariables()
 
-/**
- * Medusa Cloud-related environment variables
- */
 const S3_HOSTNAME = process.env.MEDUSA_CLOUD_S3_HOSTNAME
 const S3_PATHNAME = process.env.MEDUSA_CLOUD_S3_PATHNAME
 
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
+  experimental: {
+    dynamicIO: false,
+  },
   logging: {
     fetches: {
       fullUrl: true,
@@ -44,13 +42,7 @@ const nextConfig = {
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
       ...(S3_HOSTNAME && S3_PATHNAME
-        ? [
-            {
-              protocol: "https",
-              hostname: S3_HOSTNAME,
-              pathname: S3_PATHNAME,
-            },
-          ]
+        ? [{ protocol: "https", hostname: S3_HOSTNAME, pathname: S3_PATHNAME }]
         : []),
     ],
   },
